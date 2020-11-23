34e6aa332451888a79c08507cfa0d922041956fa
ptnk1995 <tiennp@concrete-corp.com>
2020-11-23 10:16:12 +0700

    update page top

--- template/dist/assets/js/main.js
+++ template/dist/assets/js/main.js
@@ -13,10 +13,10 @@
 			particlesJS("banner-particles", {
 				particles: {
 					number: { value: 20, density: { enable: true, value_area: 800 } },
-					color: { value: "#ffffff" },
+					color: { value: "#ffa93d" },
 					shape: {
 						type: "circle",
-						stroke: { width: 0, color: "#000000" },
+						stroke: { width: 8, color: "#ffa93d" },
 						polygon: { nb_sides: 5 },
 						image: { src: "img/github.svg", width: 100, height: 100 }
 					},
@@ -26,16 +26,16 @@
 						anim: { enable: false, speed: 0.8, opacity_min: 0.1, sync: false }
 					},
 					size: {
-						value: 3,
+						value: 2,
 						random: true,
 						anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
 					},
 					line_linked: {
 						enable: true,
-						distance: 155,
-						color: "#ffffff",
+						distance: 255,
+						color: "#ffa93d",
 						opacity: 0.4,
-						width: 1
+						width: 2
 					},
 					move: {
 						enable: true,
